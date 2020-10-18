09dd5f137eca8e1495eea9d357c642a9f5bebb83
Yesika Castillo <ylcastillo29@hotmail.com>
2020-10-17 22:38:12 -0500

    instalacion de libreria firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import Formulario from './components/formulario'
+import { useFirebaseApp } from 'reactfire'
 
 function App() {
+  const firebase = useFirebaseApp()
+  console.log({ firebase })
+
   return (
     <div className="container mt-5">
       <Formulario />
---
+++ src/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDgrw1HCtevIdNgzkVxUxV4N3BxTl7zKCg',
+  authDomain: 'clientes-a8712.firebaseapp.com',
+  databaseURL: 'https://clientes-a8712.firebaseio.com',
+  projectId: 'clientes-a8712',
+  storageBucket: 'clientes-a8712.appspot.com',
+  messagingSenderId: '504275770458',
+  appId: '1:504275770458:web:9204028069149881c98c63',
+  measurementId: 'G-938TKTFEM3',
+}
+
+export default firebaseConfig
